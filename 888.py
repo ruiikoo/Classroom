2fa6039898daca52a91cb3b295fdb90bd4076fa5
ruiikoo <ruslanabdyreyimovras@gmail.com>
2026-04-30 16:51:39 -0500

    Add files via upload

---
+++ 888.py
@@ -0,0 +1,170 @@
+# Declare 2 numbers and calculate their sum
+# Print result
+a=2
+b=5
+sum=a+b
+print(sum)
+
+
+# Declare a string
+# Reverse the string
+# Print the reversed string
+string='Almaty'
+reverse_string=string[::-1]
+print(reverse_string)
+
+
+# Declare a string
+# Calculate the length of the string
+# Print the length
+string='Astana'
+string_length=len(string)
+print(string_length)
+
+
+# Declare 2 strings
+# Concatenate the strings
+# Print the concatenated string
+string1='I am'
+string2='Turkmen'
+connected= string1+' '+string2
+print(connected)
+
+
+# Declare a character
+# Check if the character is a vowel
+# Print the result
+character='a'
+if character in 'aeiou':
+    result='Vowel'
+else:
+    result='Not vowel'
+print(result)
+
+
+# Declare a string
+# Swap the first and last characters of the string
+# Print the modified string
+s='Avengers'
+modified_string=s[-1]+s[1:-1]+s[0]
+print(modified_string)
+
+
+# Declare a string
+# Convert the string to uppercase
+# Print the uppercase string
+s='ruiikoo'
+uppercase_string=s.upper()
+print(uppercase_string)
+
+
+# Declare the length and width of a rectangle
+# Calculate the area of the rectangle
+# Print the area
+length=10
+width=4
+rectangle_area=length*width
+print(rectangle_area)
+
+
+# Declare a number
+# Check if the number is even
+# Print the result
+number=7
+if number%2==0:
+    result='Even'
+else:
+    result='Odd'
+print(result)
+
+
+# Declare a string
+# Extract the first three characters of the string
+# Print the extracted characters
+string='Thanos'
+exctracted=string[:3]
+print(exctracted)
+
+
+# Declare 2 variables, name and age
+# Use string interpolation to create a message
+# Print the message
+name='Tai Lung'
+age='27'
+message=f'{name} is {age} years old.'
+print(message)
+
+
+# Declare a string
+# Extract the characters from index 2 to 5 (inclusive)
+# Print the extracted characters
+string='Kyzylorda'
+extracted=string[2:6]
+print(extracted)
+
+
+# Declare a number as a string
+# Convert the string to an integer
+# Print the integer
+number='7'
+int_number=int(number)
+print(int_number)
+
+
+# Declare a string
+# Repeat the string 3 times
+# Print the repeated string
+string='Ruiikoo is the coolest developer'
+n=1
+while n<4:
+    print(string)
+    n+=1
+
+
+# Declare 2 numbers
+# Calculate the quotient and remainder
+# Print the quotient and remainder
+num1=9
+num2=4
+quotient=num1//num2
+remainder=num1%num2
+print(quotient, remainder)
+
+
+# Declare 2 numbers
+# Calculate the result of float division
+# Print the result
+num1=9
+num2=4
+float_division=num1/num2
+print(float_division)
+
+
+# Declare a string
+# Use a string method to count the occurrences of a character
+# Print the count
+string='Karaganda'
+character_count=string.count('a')
+print(character_count)
+
+
+# Declare a string with double quotes inside
+# Use escape sequences to include the quotes
+# Print the string
+string='My real name is Ruslan, but my nickname on all social media is "Ruiikoo".'
+print(string)
+
+
+# Declare a multi-line string
+# Print the multi-line string
+string='Cristiano \nRonaldo \nSiuuuuu'
+print(string)
+
+
+# Declare 2 numbers, base and exponent
+# Calculate the result of base to the power of exponent
+# Print the result
+base=3
+exponent=2
+result=base**exponent
+print(result)
